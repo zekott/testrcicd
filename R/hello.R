756cd5ab0e7e75215cfd83767c3994a57db408c0
zeko <zech@me.org>
2018-10-13 16:38:06 -0400

    Initial Commit and changes to hello.R

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,18 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#' Show the CI/CD functionality
+#'
+#' @param x The name of the Person who is being welcomed
+#'
+#' @return The output from \code{\link{print}}
+#'
+#' @export
+#'
+#' @examples
+#' hello("Zech")
+#' \dontrun{
+#' hello("Rose")
+#' }
+#'
 
-hello <- function() {
-  print("Hello, world!")
+hello <- function(x) {
+  print(paste0("Hello, ", x, "Welcome to the world of CI/CD!"))
 }
---
+++ README.md
@@ -0,0 +1,24 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+testrcicd
+=========
+
+This package was built to demonstrate the functionality of CI/CD in R.
+
+Installation
+------------
+
+You can install the released version of testrcicd from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("testrcicd")
+```
+
+Example
+-------
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+## basic example code
+```
